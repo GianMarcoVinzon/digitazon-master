30db184c965c45e1330df20d82fe32853b179c35
GianMarcoVinzon <gianmarcov07@gmail.com>
2023-05-15 09:41:47 +0200

    updates

---
+++ glossario-react/src/components/es-reactRouter.js
@@ -0,0 +1,5 @@
+// utilizzo di librerie non viste in classe:
+//  si richiede di creare una applicazione React che sfrutti React Router.
+// A sinistra deve esserci un menu di navigazione,
+//  e in centro il dettaglio del componente attuale,
+//   quando cliccate una voce del menu a sx, il dettaglio cambia.
